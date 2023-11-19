74f90c11bb6b3965050dd01742d96e34868977ad
Igor Ramos Gonçalves <igor.ramos@live.com>
2023-11-19 15:31:07 -0300

    Digitando em campos e clicando em elementos - ex extra 2

--- testes-automatizados-cypress-basico/cypress/integration/CAC-TAT.spec.js
+++ testes-automatizados-cypress-basico/cypress/integration/CAC-TAT.spec.js
@@ -9,7 +9,7 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
   });
 
-  it.only("preenche os campos obrigatórios e envia o formulário", function () {
+  it("preenche os campos obrigatórios e envia o formulário", function () {
     const longText =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
     cy.get("#firstName").type("Igor");
@@ -18,5 +18,15 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get("#open-text-area").type(longText, { delay: 0 });
     cy.get("button[type='submit']").click();
     cy.get(".success").should("be.visible");
+  });
+
+  it("exibe mensagem de erro ao submeter o formulário com um e-mail com formatação inválida", function () {
+    cy.get("#firstName").type("Igor");
+    cy.get("#lastName").type("Ramos");
+    cy.get("#email").type("igortestegmail.com");
+    cy.get("#open-text-area").type("Teste");
+    cy.get("button[type='submit']").click();
+
+    cy.get(".error").should("be.visible");
   });
 });
