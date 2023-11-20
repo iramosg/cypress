0726405e8d6d97b0cc7615a33bcc0f5880cbba94
Igor Ramos Gonçalves <igor.ramos@live.com>
2023-11-19 22:51:24 -0300

    Desafio: encontre o gato

--- testes-automatizados-cypress-basico/cypress/integration/CAC-TAT.spec.js
+++ testes-automatizados-cypress-basico/cypress/integration/CAC-TAT.spec.js
@@ -199,4 +199,10 @@ describe("Central de Atendimento ao Cliente TAT", function () {
       expect(body).to.include("CAC TAT");
     });
   });
+
+  it.only("encontra o gato escondido", function () {
+    cy.get("#cat").invoke("show").should("be.visible");
+    cy.get("#title").invoke("text", "CAT TAT");
+    cy.get("#subtitle").invoke("text", "Eu ❤️ gatos");
+  });
 });
