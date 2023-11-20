928a8341f980b0161544cb6f82e9e3788bbbc204
Igor Ramos Gonçalves <igor.ramos@live.com>
2023-11-19 22:27:10 -0300

    Avançando no uso do Cypress - Lodash

--- testes-automatizados-cypress-basico/cypress/integration/privacy.spec.js
+++ testes-automatizados-cypress-basico/cypress/integration/privacy.spec.js
@@ -1,3 +1,5 @@
-it.only("testa a página da política de privacidade de forma independente", function () {
-  cy.visit("./src/privacy.html");
+Cypress._.times(3, function () {
+  it.only("testa a página da política de privacidade de forma independente", function () {
+    cy.visit("./src/privacy.html");
+  });
 });
